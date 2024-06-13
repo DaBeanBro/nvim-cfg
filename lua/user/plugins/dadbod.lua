1f03a86f19871c202dac2ea743a9d53aa6cd33bc
Peter Kilian <143734154+DaBeanBro@users.noreply.github.com>
2024-06-14 01:24:17 +0200

    dadbod.lua

---
+++ lua/user/plugins/dadbod.lua
@@ -0,0 +1,47 @@
+return {
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	opts = {
+		db_competion = function()
+			---@diagnostic disable-next-line
+			require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+		end,
+	},
+	config = function(_, opts)
+		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"sql",
+			},
+			command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"sql",
+				"mysql",
+				"plsql",
+			},
+			callback = function()
+				vim.schedule(opts.db_completion)
+			end,
+		})
+	end,
+	keys = {
+		{ "<leader>dt", "<cmd>DBUIToggle<cr>", desc = "Toggle UI" },
+		{ "<leader>df", "<cmd>DBUIFindBuffer<cr>", desc = "Find Buffer" },
+		{ "<leader>dr", "<cmd>DBUIRenameBuffer<cr>", desc = "Rename Buffer" },
+		{ "<leader>dq", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
+	},
+}
