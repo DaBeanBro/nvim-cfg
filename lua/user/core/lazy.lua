6bbce90626f3962406c377335bb5a76a9ce640ea
Peter Kilian <143734154+DaBeanBro@users.noreply.github.com>
2024-06-17 01:46:36 +0200

    Some lazy improvements

--- lua/user/core/lazy.lua
+++ lua/user/core/lazy.lua
@@ -1,14 +1,36 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
+		"--single-branch",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
 		lazypath,
 	})
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
-require("lazy").setup({ import = "user.plugins" })
+require("lazy").setup("user.plugins", {
+	defaults = { lazy = true },
+	install = { colorscheme = { "tokyonight" } },
+	checker = { enabled = true },
+	concurrency = 5,
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	debug = false,
+	ui = {
+		border = "rounded",
+	},
+})
+
+vim.keymap.set("n", "<leader><leader>l", "<cmd>:Lazy<cr>")
