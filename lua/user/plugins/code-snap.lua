6760746a84d44f10e911a6e990e08d39bbea4325
Peter Kilian <143734154+DaBeanBro@users.noreply.github.com>
2024-06-17 01:00:43 +0200

    Code-snap

---
+++ lua/user/plugins/code-snap.lua
@@ -0,0 +1,17 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = "make build_generator",
+	keys = {
+		{ "<leader>cs", ":CodeSnap<CR>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+		{ "<leader>css", ":CodeSnapSave<CR>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+	},
+	config = function()
+		require("codesnap").setup({
+			save_path = "~/Pictures",
+			has_breadcrumbs = true,
+			watermark = "",
+			bg_color = "#535c68",
+			title = "vraton.dev from CodeSnap.nvim",
+		})
+	end,
+}
