c6e39524e35dac00b8928752ff8371477bad4342
Peter Kilian <143734154+DaBeanBro@users.noreply.github.com>
2024-06-19 19:22:10 +0200

    YAY

--- lua/user/plugins/live-server.lua
+++ lua/user/plugins/live-server.lua
@@ -1,7 +1,21 @@
-return {
-	"barrett-ruth/live-server.nvim",
-	build = "pnpm add -g live-server",
-	event = "VimEnter",
-	cmd = { "LiveServerStart", "LiveServerStop" },
-	config = true,
+local M = {
+	"lethc/bracey.vim",
+	build = "npm install --prefix server",
+	dependencies = {
+		{
+			"barrett-ruth/live-server.nvim",
+			build = "pnpm add -g live-server",
+			event = "VimEnter",
+			cmd = { "LiveServerStart", "LiveServerStop" },
+			config = true,
+		},
+		"ray-x/web-tools.nvim",
+	},
 }
+
+function M.config()
+	require("live-server").setup()
+	require("web-tools").setup()
+end
+
+return M
